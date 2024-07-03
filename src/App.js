68e1a18edd97f0fdd8623f25fc6aea8dbaebf5e9
Alex G <agulley@protonmail.com>
2024-07-02 22:07:28 -0400

    created basic page structure | added babel dev dependencies | deleted extraneous files from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html lang="en">
+      <head>
+        <title>Watch List</title>
+      </head>
+      <body>
+        <header>
+          <h1>
+            <a href="#">Watch List</a>
+          </h1>
+        </header>
+        <main>
+          <p>Stuff goes here</p>
+        </main>
+        <footer>
+          <p>&copy; Watch List 2024</p>
+        </footer>
+      </body>
+    </html>
   );
 }
 
