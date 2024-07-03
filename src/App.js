03373bbc2f8f89f241337df11235c8a12c3b0dde
Alex G <agulley@protonmail.com>
2024-07-02 22:33:28 -0400

    modified structure to work with react better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-function App() {
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { useEffect } from 'react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+function App({ signOut, user }) {
+  useEffect(() => {
+    document.title = "Watch List";
+  }, []);
+
   return (
-    <html lang="en">
-      <head>
-        <title>Watch List</title>
-      </head>
-      <body>
-        <header>
+    <div className='app-container'>
+      <div className='body'>
+        <div className='header'>
           <h1>
             <a href="#">Watch List</a>
           </h1>
-        </header>
-        <main>
-          <p>Stuff goes here</p>
-        </main>
-        <footer>
+        </div>
+        <div className='main'>
+          <p>Welcome, {user.signInDetails.loginId}!</p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+        <div className='footer'>
           <p>&copy; Watch List 2024</p>
-        </footer>
-      </body>
-    </html>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
