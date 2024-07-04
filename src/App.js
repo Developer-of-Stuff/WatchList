4fd6bdad80eec6a0a30eebb5ef2949625f3039a3
Alex G <agulley@protonmail.com>
2024-07-03 21:52:13 -0400

    added loadEntries function to useEffect to load after authentication

--- src/App.js
+++ src/App.js
@@ -18,20 +18,104 @@ async function retrieveEntries() {
   return entriesList.data.listMedia;
 }
 
-const initialMediaEntries = (await retrieveEntries()).items;
+async function addEntry(formDataObj, user) {
+  const addMediaResult = await appsyncClient.graphql({
+    query: mutations.createMedia,
+    variables: {
+      input: {
+        name: formDataObj.title,
+        medium: formDataObj.medium === 'movie' ? 'MOVIE' : 'SHOW',
+        creatorId: user.username,
+        watched: false
+      }
+    }
+  });
+
+  console.log(addMediaResult);
+}
+
+async function updateEntry(id, watched, formDataObj=null) {
+  let input;
+
+  if (formDataObj) {
+    input = {
+      id: id,
+      name: formDataObj.title,
+      medium: formDataObj.medium === 'movie' ? 'MOVIE' : 'SHOW',
+      watched: formDataObj.watched
+    }
+  } else {
+    input = {
+      id: id,
+      watched: watched
+    }
+  }
+
+  const updateMediaResult = await appsyncClient.graphql({
+    query: mutations.updateMedia,
+    variables: {
+      input: input
+    }
+  });
+
+  console.log(updateMediaResult);
+}
+
+async function deleteEntry(id) {
+  const deleteMediaResult = await appsyncClient.graphql({
+    query: mutations.deleteMedia,
+    variables: {
+      input: {
+        id: id
+      }
+    }
+  });
+
+  console.log(deleteMediaResult);
+}
 
 function createEntries(mediaEntries) {
   const entries = [];
+  const handleCheck = async (e) => {
+    const target = e.target;
+    const entryId = target.parentNode.parentNode.id;
+    try {
+      await updateEntry(entryId, target.checked);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async (e) => {
+    const target = e.target;
+    const entryId = target.parentNode.parentNode.id;
+    try {
+      await deleteEntry(entryId);
+      target.parentNode.parentNode.remove();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   for (let i = 0; i < mediaEntries.length; i++) {
+    const wasWatched = <input type='checkbox' onClick={handleCheck} defaultChecked />
+    const notWatched = <input type='checkbox' onClick={handleCheck}/>
     entries.push(
-      <>
-        <ul key={i} id={mediaEntries[i].id}>
-          <li className='entry entry-name'>{mediaEntries[i].name}</li>
-          <li className='entry entry-medium'>{mediaEntries[i].medium}</li>
-          <li className='entry entry-createdAt'>{mediaEntries[i].createdAt}</li>
-          <li className='entry entry-creatorId'>{mediaEntries[i].creatorId}</li>
-        </ul>
-      </>
+      <ul key={i} id={mediaEntries[i].id}>
+        <li className='entry entry-name'>{mediaEntries[i].name}</li>
+        <li className='entry entry-medium'>{mediaEntries[i].medium}</li>
+        <li className='entry entry-createdAt'>{mediaEntries[i].createdAt}</li>
+        <li className='entry entry-creatorId'>{mediaEntries[i].creatorId}</li>
+        <li className='entry entry-watched'>
+          {mediaEntries[i].watched ? wasWatched : notWatched}
+        </li>
+        <li className='btn update-btn'>
+          <button>Update Item</button>
+        </li>
+        <li className='btn delete-btn'>
+          <button onClick={handleDelete}>Delete Item</button>
+        </li>
+      </ul>
     );
   }
 
@@ -40,10 +124,18 @@ function createEntries(mediaEntries) {
 
 
 function App({ signOut, user }) {
-  const [entries, setEntries] = useState(() => createEntries(initialMediaEntries));
+  
+  const [entries, setEntries] = useState();
 
   useEffect(() => {
     document.title = "Watch List";
+    async function loadEntries() {
+      const entries = (await retrieveEntries()).items;
+      setEntries(createEntries(entries));
+      console.log("Loaded entries")
+    }
+
+    loadEntries();
   }, []);
 
 
@@ -55,25 +147,13 @@ function App({ signOut, user }) {
     let formDataObj = Object.fromEntries(formData.entries())
     console.log(formDataObj);
 
-    if (formDataObj.title == '') {
+    if (formDataObj.title === '') {
       alert('Title is required')
     }
 
     else {
       try {
-        const addMediaResult = await appsyncClient.graphql({
-          query: mutations.createMedia,
-          variables: {
-            input: {
-              name: formDataObj.title,
-              medium: formDataObj.medium == 'movie' ? 'MOVIE' : 'SHOW',
-              creatorId: user.username,
-              watched: false
-            }
-          }
-        });
-
-        console.log(addMediaResult);
+        await addEntry(formDataObj, user);
         
         const newEntries = (await retrieveEntries()).items;
 
@@ -83,6 +163,7 @@ function App({ signOut, user }) {
       }
     }
   };
+
 
   return (
     <div className='app-container'>
